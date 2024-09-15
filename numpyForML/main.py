64e95f031d7696289198c7a3abf44fa43e1af31b
hmd <hamedikhalili228@gmail.com>
2024-09-16 00:12:38 +0330

    Im trying to learn an introduce how to use and call py classes and methods

---
+++ numpyForML/main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+import mySelf
+
+
+object1=mySelf.calc()
+pp=mySelf
+
+print("Sum : ",object1.sum(20,30))
+print("Min : ",object1.min(20,10))
+print(object1.sayHi("Hamed"))
+
+# print("MySelf calss variable is : ",pp.myVariable)
+# print("Min : ",object1.num(90,30))
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+
---
+++ numpyForML/mySelf.py
@@ -0,0 +1,14 @@
+
+
+class calc:
+    def sum(self,a,b):
+        return a+b
+
+
+    def min(self,a,b):
+        return a-b
+
+    def sayHi(self,name):
+        return "Hi dear \t"+ name
+
+myVariable = 1000
