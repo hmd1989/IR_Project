eb3037fd1637633e5833391c1c24a1545a0c7340
hmd <hamedikhalili228@gmail.com>
2024-09-16 00:09:32 +0330

    second python sample commit

---
+++ pyObjAndClasses/FileA.py
@@ -0,0 +1,22 @@
+import datetime
+
+
+class A:
+    def say_Hi(self):
+        # print("SayHi Method of Class A is calling")
+        # print("Hi dear ", name)
+        return "hi there ... "
+
+
+
+    def sayGoodBye(self):
+        return "Goodbye there ..."
+
+    def welcom(self,name):
+        return "Hi dear : \t"+name
+
+
+
+
+
+
---
+++ pyObjAndClasses/main.py
@@ -0,0 +1,12 @@
+# for using of other class in other py file you should import their files, like Packages
+
+import FileA
+
+
+obj1=FileA.A()
+
+
+
+print( obj1.welcom("hamed"))
+print( obj1.say_Hi())
+print( obj1.sayGoodBye())
